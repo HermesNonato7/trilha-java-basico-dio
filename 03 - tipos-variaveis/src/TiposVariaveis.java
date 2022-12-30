48a66a9f68023cf90acd751699213c103463a536
HermesNonato7 <hermes.nonato7@hotmail.com>
2022-12-30 03:03:28 -0300

    tipos de variaveis

--- 03 - tipos-variaveis/src/TiposVariaveis.java
+++ 03 - tipos-variaveis/src/TiposVariaveis.java
@@ -2,5 +2,11 @@ public class TiposVariaves {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500.33;
         System.out.println(salarioMinimo);
+
+        /*
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+        */
     }
 }
