705523a65bd58a123848d59034c6e52b57f80754
HermesNonato7 <hermes.nonato7@hotmail.com>
2022-12-31 18:05:33 -0300

    operadores concatenação

--- 04 - operadores/src/OperadoresConcatenacao.java
+++ 04 - operadores/src/OperadoresConcatenacao.java
@@ -3,33 +3,33 @@ public class OperadoresConcatenacao {
 
         String nomeCompleto = "LINGUAGEM" + "JAVA";
 
-        System.out.println(nomeCompleto);
+            System.out.println(nomeCompleto);
 
         // qual o resultado das expressoes abaixo?
         // Imprimir utilizando o Debug
 
         String concatenacao = "?";
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
 
         concatenacao = 1 + 1 + 1 + "1";
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
 
         concatenacao = 1 + "1" + 1 + 1;
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
 
         concatenacao = 1 + "1" + 1 + "1";
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
 
         concatenacao = "1" + 1 + 1 + 1;
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
 
         concatenacao = "1" + (1 + 1 + 1);
 
-        System.out.println(concatenacao);
+            System.out.println(concatenacao);
     }
 }
