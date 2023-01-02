c6c5386ffbbb61a878d6514c4458623874f09382
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-02 00:37:13 -0300

    Projeto Smart Tv

---
+++ 06 - projeto-Smart-Tv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada ? " + smartTv.ligada);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> Tv ligada ? " + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo Status -> Tv ligada ? " + smartTv.ligada);
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+    }
+}
