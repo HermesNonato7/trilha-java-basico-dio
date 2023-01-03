fc9c46a9175b8e8c74df0097d3f39b5e5a4552f8
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-03 01:44:47 -0300

    java doc

---
+++ 08 - documentacao java/src/Calculadora.java
@@ -0,0 +1,33 @@
+
+/**
+ * Exemplo de documentação em Java.
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author Hermes Nonato
+ * @version 1.0
+ * @since 03/01/2023
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+    }
+
+/**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm   este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+
+        // No terminal execute o comando abaixo
+
+       // javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+    }
+}
