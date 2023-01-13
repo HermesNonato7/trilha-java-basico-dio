1f81a7d11908cbc852bff6cb23bdea731239bf20
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-13 13:32:43 -0300

    Exercício 5 - Tabuada- Laço for

--- 11 - Loops e Arrays - Exercicios/src/Ex5_Tabuada.java
+++ 11 - Loops e Arrays - Exercicios/src/Ex5_Tabuada.java
@@ -24,6 +24,7 @@ public class Ex5_Tabuada {
 
         for(int i = 1; i <= 10; i++) {        
             System.out.println(tabuada + " X " + i  + " = " + (tabuada*i));
+            
         }
     }
 }
