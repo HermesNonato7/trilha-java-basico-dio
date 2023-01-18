cae0f24d8c17391d3334197a5c2c8f6e9e83d10e
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-18 01:05:29 -0300

    Exercício 3- Array- Laço foreach, randon

---
+++ 12 - Arrays - Exercicios/src/Ex3_NumerosAleatorios.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+/*
+ * Faça um programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os em um vetor.
+ * Ao final mostre os números e seus sucessores. 
+ */
+
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        Random randon = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = randon.nextInt(100);
+            numerosAleatorios[i] = numero; 
+        }
+
+        System.out.print("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+        
+        System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero-1) + " ");
+        }
+        
+        System.out.print("\nSucessores dos Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero+1) + " ");
+        }
+
+    }
+}
