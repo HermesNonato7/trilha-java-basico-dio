e9b0ed8053eda34968d5fe0afad40c2cbf65d686
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-22 16:12:40 -0300

    Exercíco 2 - Métodos- Sobracargas

---
+++ 13 - Métodos - Exercícios/src/sobrecargaMetodos/Quadrilatero.java
@@ -0,0 +1,24 @@
+package sobrecargaMetodos;
+
+public class Quadrilatero {
+    public static void area(double lado) {
+
+        System.out.println("Area do quadrado: " + lado * lado);
+    }
+
+    public static void area(double lado1, double lado2) {
+
+        System.out.println("Area retângulo: " + lado1 * lado2);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println("Area do trapézio: " + ((baseMaior + baseMenor) * altura) / 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println("Area losango: " + (diagonal1 * diagonal2) / 2);
+    }
+
+}
