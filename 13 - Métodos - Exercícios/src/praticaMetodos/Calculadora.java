342ee17c60ecefc5f9164b77cb3d7a65bae719d0
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-22 13:17:37 -0300

    Exercício 1 - Criação de Métodos na prática

---
+++ 13 - Métodos - Exercícios/src/praticaMetodos/Calculadora.java
@@ -0,0 +1,27 @@
+package praticaMetodos;
+
+/**
+ * Classe de exepmlo da Aula 1 de Métodos.
+ * Criação de métodos 
+ */
+public class Calculadora {
+    public static void soma(double numero1, double numero2) {
+        double resultado = numero1 + numero2;
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + resultado);
+    }
+
+    public static void subtracao(double numero1, double numero2) {
+        double resultado = numero1 - numero2;
+        System.out.println("A subtração de " + numero1 + " menos " + numero2 + " é " + resultado);
+    }
+
+    public static void multiplicacao(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + resultado);
+    }
+
+    public static void divisao(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + resultado);
+    }
+}
