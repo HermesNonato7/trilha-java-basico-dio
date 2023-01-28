6f4fd2349520fbc9b97b69c6266cde3a8c43e0d0
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-28 20:35:45 -0300

    POO - Pacotes e Visibilidade

---
+++ 16 - Pacotes e Visibilidade/src/one/digitalinnovation/oo/ExemploPacotes.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.oo;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+/**
+ * Classe para exercíco prático de criação de pacotes e visibilidade da Aula 5- POO
+ */
+class ExemploPacotes {
+
+    ExemploPacotes1 outroPacote;
+}
