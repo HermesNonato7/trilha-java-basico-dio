157dbd683b18afffa20e10dd58deea60c44efd87
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-28 20:35:16 -0300

    POO- Pacotes e Visibilidade

---
+++ 16 - Pacotes e Visibilidade/src/one/digitalinnovation/oo/outropacote/ExemploPacotes1.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.oo.outropacote;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes2;
+
+/**
+ * Classe de exemplo para o exercício da Aula 5 de Orientação a Objetos.
+ */
+public class ExemploPacotes1 {
+
+ExemploPacotes2 exemploPacotes2;
+}
+
---
+++ 16 - Pacotes e Visibilidade/src/one/digitalinnovation/oo/outropacote/ExemploPacotes2.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.oo.outropacote;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+public class ExemploPacotes2 {
+
+    ExemploPacotes1 exemploPacotes1; 
+}
+
