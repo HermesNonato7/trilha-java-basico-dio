d1c1ab63d3697353083f2209a1f4cf3b08c627ad
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-01-27 21:13:41 -0300

    Exercício Interface- POO

---
+++ 15 - POO- Herança, Associação e Interface/src/ExercicioInterface/OperacaoMatematica.java
@@ -0,0 +1,19 @@
+package ExercicioInterface;
+
+/**
+ * Interface de exemplo da Aula 4- Programação Orientada a Objetos
+ */
+
+interface OperacaoMatematica {       
+    
+
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+    
+}
+
