619acbc872450745c1a9795e61db7ac8808f42f5
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-02-05 00:55:37 -0300

    Treinando Debug

--- _18 - Debugging/src/CalculadoraDeMedias.java
+++ _18 - Debugging/src/CalculadoraDeMedias.java
@@ -5,17 +5,17 @@ public class CalculadoraDeMedias {
         Scanner scan = new Scanner(System.in);
         String[] alunos = {"Lucas", "Lunah", "Ariana", "Hermes"};
 
-        int media = calculaMediaDaTurma(alunos, scan);
+        double media = calculaMediaDaTurma(alunos, scan);
 
-        System.out.printf("Media da turma %d" , media);
+        System.out.printf("Media da turma %.1f" , media);
     }
 
-    public static int calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
 
-        int soma = 0;
+        double soma = 0;
         for(String aluno : alunos) {
             System.out.printf("Nota do aluno %s: ", aluno);
-            int nota = scanner.nextInt();
+            double nota = scanner.nextInt();
             soma += nota;
         }
 
