ca0c1c2f55f2af7cc2fd72a0341c5819721e9371
HermesNonato7 <hermes.nonato7@hotmail.com>
2023-03-13 22:32:32 -0300

    Integrando o Spring Data JPA com o PostgreSQL

---
+++ 20 - Springboot/spring-configuration-properties/src/main/java/dio/springboot/app/SistemaMensagem.java
@@ -0,0 +1,18 @@
+package dio.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+    @Autowired
+    private Rementente rementente;
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Mensagem enviada por: " + rementente.getNome()
+                + "\nE-mail:" + rementente.getEmail() + "\nCom telefones para contato: "
+                + rementente.getTelefones());
+        System.out.println("Seu cadastro foi aprovado");
+    }
+}
